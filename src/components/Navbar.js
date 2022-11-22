be46da4bee21b6a0fc89e5a535c4e6d78eab058a
Hau1006 <vanhau1061998@gmail.com>
2022-11-22 16:22:56 +0700

    add structure nav component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
 import React from "react";
 import Wrapper from "../assets/wrappers/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from "react-icons/fa";
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <button type="button" className="toggle-btn" onClick={() => console.log("toggle ")}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          {/* <Logo /> */}
+          <h3 className="logo-text">Dashboard</h3>
+        </div>
+        <div className="btn-container">
+          <button type="button" className="btn" onClick={() => console.log("togle click")}>
+            <FaUserCircle />
+            {user?.user?.name}
+            <FaCaretDown />
+          </button>
+          <div className="dropdown show-dropdown">
+            <button
+              type="button"
+              className="dropdown-btn"
+              onClick={() => console.log("logout user")}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
